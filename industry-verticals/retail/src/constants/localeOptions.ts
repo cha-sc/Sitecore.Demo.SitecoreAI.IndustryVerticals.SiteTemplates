1e3893f69be20795a42df6accdd86a3594085b6a
Chris Haddad <cha@sitecore.net>
2026-02-23 13:31:00 -0500

    updated locale options to use CAD

--- industry-verticals/retail/src/constants/localeOptions.ts
+++ industry-verticals/retail/src/constants/localeOptions.ts
@@ -1,7 +1,7 @@
 import { LocaleOption } from '@/types/locale';
 
 export const localeOptions: LocaleOption[] = [
-  { code: 'en', label: 'EN | USD', currency: 'USD', currencySymbol: '$' },
-  { code: 'fr-FR', label: 'FR | EUR', currency: 'EUR', currencySymbol: '€' },
+  { code: 'en', label: 'EN | CAD', currency: 'CAD', currencySymbol: '$' },
+  { code: 'fr-FR', label: 'FR | CAD', currency: 'CAD', currencySymbol: '$' },
   { code: 'es-ES', label: 'ES | EUR', currency: 'EUR', currencySymbol: '€' },
 ];
