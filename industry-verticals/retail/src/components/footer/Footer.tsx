c09a3dba636132d7b28aa4fa631cd3514782b278
cha-sc <cha@sitecore.net>
2026-05-11 14:26:21 -0400

    footer and color updates

--- industry-verticals/retail/src/components/footer/Footer.tsx
+++ industry-verticals/retail/src/components/footer/Footer.tsx
@@ -73,7 +73,7 @@ export const Default = (props: FooterProps) => {
 
   return (
     <section className={`component footer relative ${props.params.styles} overflow-hidden`} id={id}>
-      <div className="bg-background-muted">
+      <div className="bg-background-muted-dark text-background">
         <div className="container grid gap-12 py-28.5 lg:grid-cols-[1fr_3fr]">
           <div className="flex flex-col gap-7">
             <div className="sm:max-w-34">
@@ -84,14 +84,14 @@ export const Default = (props: FooterProps) => {
           <div className="grid gap-13 sm:grid-cols-3 lg:grid-cols-5 lg:gap-5 xl:gap-12">
             {sections.map(({ key, title, content }) => (
               <div key={key}>
-                <div className="text-accent mb-8 text-lg font-bold">{title}</div>
+                <div className="text-background mb-8 text-lg font-bold">{title}</div>
                 <div className="space-y-4">{content}</div>
               </div>
             ))}
           </div>
         </div>
       </div>
-      <div className="bg-background">
+      <div className="bg-brand-black text-background">
         <div className="container flex items-center justify-between py-8.5 max-sm:flex-col max-sm:items-start max-sm:gap-10">
           <div className="max-sm:order-2">
             <Text field={props.fields.CopyrightText} />
